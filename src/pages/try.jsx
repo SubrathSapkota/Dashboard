3c3e6383c9450e39da117fb14107fb0aadf5ef8d
Subrath sapkota <subrathsapkota119@gmail.com>
2023-07-23 23:37:35 +0545

    this is just a try

--- src/pages/try.jsx
+++ src/pages/try.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const try = () => {
-  return (
-    <div>
-      this is try page
-    </div>
-  )
-}
+const trythis = () => {
+  return <div>this is try page</div>;
+};
 
-export default try
-this is try page
+export default trythis;
